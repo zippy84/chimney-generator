809d9b2a12aec0c2fc3cf47c7560588157feb2c2
Ronald Römer <rroemer@gmail.com>
2024-01-18 17:47:15 +0100

    multiple configs

--- generate.py
+++ generate.py
@@ -11,183 +11,193 @@ from vtkmodules.vtkIOGeometry import vtkSTLWriter
 from vtkmodules.vtkFiltersCore import vtkPolyDataNormals, vtkCleanPolyData
 from vtkmodules.vtkFiltersModeling import vtkLinearExtrusionFilter
 
-from vtkmodules.vtkCommonTransforms import vtkTransform
-from vtkmodules.vtkFiltersGeneral import vtkTransformPolyDataFilter
-
 from vtkbool.vtkBool import vtkPolyDataBooleanFilter
 
-a = 4.3333
-b = 6.5
+class Chimney:
+    def __init__(self, cfg):
+        self.cfg = { 'e': .15, 'f': .625, 'h': .75 }
 
-e = .15
-f = .625
+        self.cfg.update(cfg)
 
-seqs = [
-    [(2, 2, 2, 2), (3, 4, 2, 3)],
-    [(3, 2, 3), (2, 2, 2, 2, 2, 2)],
-    [(2, 2, 2, 2), (3, 2, 4, 3)],
-    [(3, 2, 3), (2, 2, 2, 2, 2, 2)]
-]
+        self.counts_a = [ sum(s_a) for s_a, _ in cfg['seqs'] ]
+        self.counts_b = [ sum(s_b) for _, s_b in cfg['seqs'] ]
 
-h = .75
+        assert self.counts_a.count(self.counts_a[0]) == len(self.counts_a)
+        assert self.counts_b.count(self.counts_b[0]) == len(self.counts_b)
 
-div_a = 1
-div_b = 2
+        assert cfg['s'] >= 0 and cfg['s'] <= cfg['a']
 
-counts_a = [ sum(s_a) for s_a, _ in seqs ]
-counts_b = [ sum(s_b) for _, s_b in seqs ]
+    @staticmethod
+    def transform(line, ang, dx, dy):
+        tranformed = []
 
-assert counts_a.count(counts_a[0]) == len(counts_a)
-assert counts_b.count(counts_b[0]) == len(counts_b)
+        for p in line:
+            x = math.cos(ang)*p[0]-math.sin(ang)*p[1]
+            y = math.sin(ang)*p[0]+math.cos(ang)*p[1]
 
-count = 20
+            tranformed.append((x+dx, y+dy))
 
-s = a
-t = 6.916667
-phi = math.pi/4
+        return tranformed
 
-assert s >= 0 and s <= a
+    @staticmethod
+    def get_line(w, e, seq):
+        x = (w+e)/sum(seq)
 
-def transform(line, ang, dx, dy):
-    tranformed = []
+        xs = [0]
 
-    for p in line:
-        x = math.cos(ang)*p[0]-math.sin(ang)*p[1]
-        y = math.sin(ang)*p[0]+math.cos(ang)*p[1]
+        for c in seq[:-1]:
+            xs.append(xs[-1]+c*x)
 
-        tranformed.append((x+dx, y+dy))
+        line = []
 
-    return tranformed
+        for s, l in zip(xs, map(lambda c: c*x, seq)):
+            line.extend([(s, 0), (s+l-e, 0), (s+l-e, e), (s+l, e)])
 
-def get_line(w, seq):
-    x = (w+e)/sum(seq)
+        del line[-3:]
 
-    xs = [0]
+        return line
 
-    for c in seq[:-1]:
-        xs.append(xs[-1]+c*x)
+    @staticmethod
+    def get_bricks_line(a, seq_a, b, seq_b, e):
+        line_a = Chimney.get_line(a, e, seq_a)
+        line_b = Chimney.get_line(b, e, seq_b)
 
-    line = []
+        pl = line_a + Chimney.transform(line_b, math.pi/2, a, 0) + Chimney.transform(line_a, math.pi, a, b) + Chimney.transform(line_b, 3*math.pi/2, 0, b)
 
-    for s, l in zip(xs, map(lambda c: c*x, seq)):
-        line.extend([(s, 0), (s+l-e, 0), (s+l-e, e), (s+l, e)])
+        return pl
 
-    del line[-3:]
+    @staticmethod
+    def append_z(line, z=0):
+        return [ (*pt, z) for pt in line ]
 
-    return line
+    @staticmethod
+    def extrude_from_line(line, x=0, y=0, z=0):
+        cell = vtkIdList()
 
-def get_bricks_line(a, seq_a, b, seq_b):
-    line_a = get_line(a, seq_a)
-    line_b = get_line(b, seq_b)
+        pts = vtkPoints()
+        pts.SetDataTypeToDouble()
 
-    pl = line_a + transform(line_b, math.pi/2, a, 0) + transform(line_a, math.pi, a, b) + transform(line_b, 3*math.pi/2, 0, b)
+        [ (pts.InsertNextPoint(pt), cell.InsertNextId(i)) for i, pt in enumerate(line) ]
 
-    print(pl)
+        pd = vtkPolyData()
+        pd.Allocate(1)
+        pd.SetPoints(pts)
+        pd.InsertNextCell(VTK_POLYGON, cell)
 
-    return pl
+        extr = vtkLinearExtrusionFilter()
+        extr.SetInputData(pd)
+        extr.SetVector(x, y, z)
 
-def append_z(line, z=0):
-    return [ (*pt, z) for pt in line ]
+        normals = vtkPolyDataNormals()
+        normals.SetInputConnection(extr.GetOutputPort())
+        normals.AutoOrientNormalsOn()
 
-def extrude_from_line(line, x=0, y=0, z=0):
-    print(line)
+        return normals
 
-    cell = vtkIdList()
+    @staticmethod
+    def union(f_a, f_b):
+        bf = vtkPolyDataBooleanFilter()
+        bf.SetInputConnection(0, f_a.GetOutputPort())
+        bf.SetInputConnection(1, f_b.GetOutputPort())
 
-    pts = vtkPoints()
-    pts.SetDataTypeToDouble()
+        return bf
 
-    [ (pts.InsertNextPoint(pt), cell.InsertNextId(i)) for i, pt in enumerate(line) ]
+    @staticmethod
+    def difference(f_a, f_b):
+        bf = vtkPolyDataBooleanFilter()
+        bf.SetInputConnection(0, f_a.GetOutputPort())
+        bf.SetInputConnection(1, f_b.GetOutputPort())
+        bf.SetOperModeToDifference()
 
-    pd = vtkPolyData()
-    pd.Allocate(1)
-    pd.SetPoints(pts)
-    pd.InsertNextCell(VTK_POLYGON, cell)
+        return bf
 
-    extr = vtkLinearExtrusionFilter()
-    extr.SetInputData(pd)
-    extr.SetVector(x, y, z)
+    def export(self, name):
+        all_seqs = []
 
-    normals = vtkPolyDataNormals()
-    normals.SetInputConnection(extr.GetOutputPort())
-    normals.AutoOrientNormalsOn()
+        while len(all_seqs) < self.cfg['count']:
+            all_seqs.extend(self.cfg['seqs'])
 
-    return normals
+            del all_seqs[self.cfg['count']:]
 
-def union(f_a, f_b):
-    bf = vtkPolyDataBooleanFilter()
-    bf.SetInputConnection(0, f_a.GetOutputPort())
-    bf.SetInputConnection(1, f_b.GetOutputPort())
+        g = self.cfg['f']-self.cfg['e']
 
-    return bf
+        bfs = []
 
-def difference(f_a, f_b):
-    bf = vtkPolyDataBooleanFilter()
-    bf.SetInputConnection(0, f_a.GetOutputPort())
-    bf.SetInputConnection(1, f_b.GetOutputPort())
-    bf.SetOperModeToDifference()
+        for i, (s_a, s_b) in enumerate(all_seqs, start=1):
+            z = -i*self.cfg['f']
 
-    return bf
+            bricks_line = Chimney.get_bricks_line(self.cfg['a'], s_a, self.cfg['b'], s_b, self.cfg['e'])
 
-all_seqs = []
+            bricks = Chimney.extrude_from_line(Chimney.append_z(bricks_line, z), z=g)
 
-while len(all_seqs) < count:
-    all_seqs.extend(seqs)
+            gap_line = [(self.cfg['e'], self.cfg['e']),
+                (self.cfg['a']-self.cfg['e'], self.cfg['e']),
+                (self.cfg['a']-self.cfg['e'], self.cfg['b']-self.cfg['e']),
+                (self.cfg['e'], self.cfg['b']-self.cfg['e'])]
 
-    del all_seqs[count:]
+            gap = Chimney.extrude_from_line(Chimney.append_z(gap_line, z+g), z=self.cfg['e'])
 
-bfs = []
+            bfs.append(Chimney.union(bricks, gap))
 
-for i, (s_a, s_b) in enumerate(all_seqs):
-    bricks_line = get_bricks_line(a, s_a, b, s_b)
+        bf = reduce(lambda a, b: Chimney.union(a, b), bfs)
 
-    bricks = extrude_from_line(append_z(bricks_line, i*f), z=f-e)
-    gap = extrude_from_line(append_z([(e, e), (a-e, e), (a-e, b-e), (e, b-e)], (i+1)*f-e), z=e)
+        clean = vtkCleanPolyData()
+        clean.SetInputConnection(bf.GetOutputPort())
 
-    bfs.append(union(bricks, gap))
+        chimney = Chimney.union(clean, Chimney.extrude_from_line(Chimney.append_z([(0, 0), (self.cfg['a'], 0), (self.cfg['a'], self.cfg['b']), (0, self.cfg['b'])], 0), z=self.cfg['h']))
 
-bf = reduce(lambda a, b: union(a, b), bfs)
+        st_a = (self.cfg['a']+self.cfg['e'])/self.counts_a[0]-self.cfg['e']/2
+        st_b = (self.cfg['b']+self.cfg['e'])/self.counts_b[0]-self.cfg['e']/2
 
-clean = vtkCleanPolyData()
-clean.SetInputConnection(bf.GetOutputPort())
+        xs = [ st_a+i*(self.cfg['a']-2*st_a)/self.cfg['div_a'] for i in range(self.cfg['div_a']+1) ]
+        ys = [ st_b+i*(self.cfg['b']-2*st_b)/self.cfg['div_b'] for i in range(self.cfg['div_b']+1) ]
 
-chimney = union(clean, extrude_from_line(append_z([(0, 0), (a, 0), (a, b), (0, b)], count*f), z=h))
+        bfs_ = [chimney]
 
-st_a = (a+e)/counts_a[0]-e/2
-st_b = (b+e)/counts_b[0]-e/2
+        for x0, x1 in zip(xs, xs[1:]):
+            for y0, y1 in zip(ys, ys[1:]):
+                out = Chimney.extrude_from_line(Chimney.append_z([(x0+st_a, y0+st_b), (x1-st_a, y0+st_b), (x1-st_a, y1-st_b), (x0+st_a, y1-st_b)], self.cfg['h']), z=-5)
 
-xs = [ st_a+i*(a-2*st_a)/div_a for i in range(div_a+1) ]
-ys = [ st_b+i*(b-2*st_b)/div_b for i in range(div_b+1) ]
+                bfs_.append(out)
 
-bfs_ = [chimney]
+        chimney_ = reduce(lambda a, b: Chimney.difference(a, b), bfs_)
 
-for x0, x1 in zip(xs, xs[1:]):
-    for y0, y1 in zip(ys, ys[1:]):
-        out = extrude_from_line(append_z([(x0+st_a, y0+st_b), (x1-st_a, y0+st_b), (x1-st_a, y1-st_b), (x0+st_a, y1-st_b)], count*f+h), z=-5)
+        off = 1
 
-        bfs_.append(out)
+        line = [(self.cfg['s'], -off, -self.cfg['t']),
+            (-off, -off, -self.cfg['t']-math.tan(self.cfg['phi'])*(self.cfg['s']+off)),
+            (-off, -off, -self.cfg['count']*self.cfg['f']-self.cfg['h']-off),
+            (self.cfg['a']+off, -off, -self.cfg['count']*self.cfg['f']-self.cfg['h']-off),
+            (self.cfg['a']+off, -off, -self.cfg['t']-math.tan(self.cfg['phi'])*(self.cfg['a']-self.cfg['s']+off))]
 
-chimney_ = reduce(lambda a, b: difference(a, b), bfs_)
+        result = Chimney.difference(chimney_, Chimney.extrude_from_line(line, y=self.cfg['b']+2*off))
 
-tra = vtkTransform()
-tra.Translate(0, 0, -(count*f+h))
+        writer = vtkSTLWriter()
+        writer.SetInputConnection(result.GetOutputPort())
+        writer.SetFileName(name)
+        writer.Update()
 
-tf = vtkTransformPolyDataFilter()
-tf.SetInputConnection(chimney_.GetOutputPort())
-tf.SetTransform(tra)
+if __name__ == '__main__':
+    cfgs = [
+        { 'a': 4.3333, 'b': 6.5, 'seqs': [
+            [(2, 2, 2, 2), (3, 4, 2, 3)],
+            [(3, 2, 3), (2, 2, 2, 2, 2, 2)],
+            [(2, 2, 2, 2), (3, 2, 4, 3)],
+            [(3, 2, 3), (2, 2, 2, 2, 2, 2)]
+        ], 'div_a': 1, 'div_b': 2, 'count': 18, 's': 4.3333, 't': 6.916667, 'phi': math.pi/4 },
+        { 'a': 3.25, 'b': 8.6666, 'seqs': [
+            [(3, 3), (2, 2, 2, 2, 2, 2, 2, 2)],
+            [(2, 2, 2), (3, 4, 4, 2, 3)],
+            [(3, 3), (2, 2, 2, 2, 2, 2, 2, 2)],
+            [(2, 2, 2), (3, 2, 4, 4, 3)],
+        ], 'div_a': 1, 'div_b': 3, 'count': 13, 's': 3.25, 't': 4.875, 'phi': math.pi/4 },
+        { 'a': 10.8333, 'b': 4.3333, 'seqs': [
+            [(3, 4, 4, 4, 2, 3), (2, 2, 2, 2)],
+            [(2, 2, 2, 2, 2, 2, 2, 2, 2, 2), (3, 2, 3)],
+            [(3, 2, 4, 4, 4, 3), (2, 2, 2, 2)],
+            [(2, 2, 2, 2, 2, 2, 2, 2, 2, 2), (3, 2, 3)]
+        ], 'div_a': 3, 'div_b': 1, 'count': 15, 's': 7.583333, 't': 1.75, 'phi': math.pi/4 }
+    ]
 
-off = 1
-
-line = [(s, -off, -t), (-off, -off, -t-math.tan(phi)*(s+off)), (-off, -off, -count*f-h-off), (a+off, -off, -count*f-h-off), (a+off, -off, -t-math.tan(phi)*(a-s+off))]
-
-result = difference(tf, extrude_from_line(line, y=b+2*off))
-
-writer = vtkPolyDataWriter()
-writer.SetInputConnection(result.GetOutputPort())
-writer.SetFileName('bricks.vtk')
-writer.Update()
-
-stl_writer = vtkSTLWriter()
-stl_writer.SetInputConnection(result.GetOutputPort())
-stl_writer.SetFileName('bricks.stl')
-stl_writer.Update()
+    for i, cfg in enumerate(cfgs):
+        Chimney(cfg).export(f'bricks{i}.stl')
